f0b36c1cf7d6c8eeefe4f46c27b3b53337260c16
raghuvulli2000 <vulli@usc.edu>
2022-11-18 16:35:59 -0800

    Horizontal View Added in info fragment

--- app/src/main/java/com/example/yelp/InfoFragment.kt
+++ app/src/main/java/com/example/yelp/InfoFragment.kt
@@ -6,12 +6,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.core.content.contentValuesOf
+import com.bumptech.glide.Glide
 import com.example.yelp.Models.DetailModel.Detail
+import com.example.yelp.databinding.FragmentInfoBinding
+import com.example.yelp.databinding.FragmentMapBinding
 import com.google.gson.Gson
 
 
 class InfoFragment : Fragment() {
 
+    private var __binding: FragmentInfoBinding? = null
+    private val binding get() = __binding
     private var detailData: Detail? = null
     var TAG = "InfoFragment"
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,7 +36,15 @@ class InfoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info, container, false)
+        __binding = FragmentInfoBinding.inflate(layoutInflater, container, false)
+        for(i in detailData!!.photos.indices) {
+            var imageView: ImageView = ImageView(context)
+            Glide.with(binding!!.root).load(detailData!!.photos[i]).into(imageView)
+       //     imageView?.layoutParams?.width = LinearLayout.LayoutParams.WRAP_CONTENT
+           binding?.linearLayout?.addView(imageView, i)
+
+        }
+        return binding?.root
     }
 
 
